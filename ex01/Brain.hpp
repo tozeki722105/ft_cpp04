bfc5be781744dc53c827fa71ac6ec9aa4a6e9d52
toshihiro ozeki <you@example.com>
2024-10-31 16:03:48 +0900

    add: ex01

---
+++ ex01/Brain.hpp
@@ -0,0 +1,22 @@
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
+
+#include <iostream>
+#include <string>
+
+class Brain
+{
+	private:
+	const static size_t maxIdeas = 100;
+	std::string ideas[maxIdeas];
+
+	public:
+	Brain();
+	~Brain();
+	Brain(const Brain& rhs);
+	Brain& operator=(const Brain& rhs);
+};
+
+
+
+#endif
