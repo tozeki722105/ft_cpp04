a7d07c587161b15a26184626765d443e0eae0db6
toshihiro ozeki <you@example.com>
2024-11-09 21:56:30 +0900

    fix: ICharacter.hppのinclude AMateria.hppがエラーになるのを解消

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,3 +1,4 @@
+#include "AMateria.hpp"
 #include "Character.hpp"
 #include "Cure.hpp"
 #include "Ice.hpp"
@@ -6,19 +7,37 @@
 int main()
 {
 	IMateriaSource* src = new MateriaSource();
+	std::cout << '\n';
+
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
+	std::cout << '\n';
+
 	ICharacter* me = new Character("me");
-	AMateria*   tmp;
+	std::cout << '\n';
+
+	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	std::cout << '\n';
+
 	ICharacter* bob = new Character("bob");
+	std::cout << '\n';
+
 	me->use(0, *bob);
 	me->use(1, *bob);
+	std::cout << '\n';
+
 	delete bob;
+	std::cout << '\n';
+
 	delete me;
+	std::cout << '\n';
+
 	delete src;
+	std::cout << '\n';
+
 	return 0;
 }
